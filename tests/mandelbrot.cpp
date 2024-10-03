e6144bb4fc6b5a35dd1dd29024d4a4474d515e9e
Anton Kesy <antonkesy@gmail.com>
2024-10-03 12:43:52 +0200

    move test prepare into namespace

--- tests/mandelbrot.cpp
+++ tests/mandelbrot.cpp
@@ -3,6 +3,7 @@
 #include "../src/mandelbrot/openmp.hpp"
 #include "../src/mandelbrot/sequential.hpp"
 
+namespace mandelbrot_visualizer::tests {
 auto TestSettings() {
   // NOLINTBEGIN(cppcoreguidelines-avoid-magic-numbers)
   return mandelbrot_visualizer::Settings{
@@ -13,16 +14,21 @@ auto TestSettings() {
       .area = {.start = {-2.0F, -1.0F}, .end = {1.0F, 1.0F}}};
   // NOLINTEND(cppcoreguidelines-avoid-magic-numbers)
 }
+};  // namespace mandelbrot_visualizer::tests
 
 // NOLINTNEXTLINE(cert-err58-cpp,cppcoreguidelines-avoid-non-const-global-variables,cppcoreguidelines-owning-memory)
 TEST(Oracle, OpenMP) {
   using mandelbrot_visualizer::OpenMPMandelbrot;
   using mandelbrot_visualizer::SequentialMandelbrot;
 
-  const auto reference = SequentialMandelbrot(TestSettings()).Compute(false);
+  const auto reference =
+      SequentialMandelbrot(mandelbrot_visualizer::tests::TestSettings())
+          .Compute(false);
   EXPECT_TRUE(reference.has_value());
 
-  const auto open_mp = OpenMPMandelbrot(TestSettings()).Compute(false);
+  const auto open_mp =
+      OpenMPMandelbrot(mandelbrot_visualizer::tests::TestSettings())
+          .Compute(false);
   EXPECT_TRUE(open_mp.has_value());
 
   // NOLINTNEXTLINE(bugprone-unchecked-optional-access)
